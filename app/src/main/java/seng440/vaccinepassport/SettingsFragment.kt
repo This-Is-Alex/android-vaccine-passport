0dee1f08b5a63852e0c206133286949ad61ebb86
Alex Hobson <aho113@uclive.ac.nz>
2021-05-03 14:46:38 +1200

    Create preferences layout using AndroidX preferences library

---
+++ app/src/main/java/seng440/vaccinepassport/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package seng440.vaccinepassport
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
