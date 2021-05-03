fcf07e76be232f332bbdda55ad70eca31c4c2a74
Alex Hobson <aho113@uclive.ac.nz>
2021-05-04 10:16:36 +1200

    Change MainActivity to Fragment+ViewModel

--- app/src/main/java/seng440/vaccinepassport/MainActivity.kt
+++ app/src/main/java/seng440/vaccinepassport/MainActivity.kt
@@ -2,10 +2,17 @@ package seng440.vaccinepassport
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import seng440.vaccinepassport.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
     }
 }
