96c782d6be1578594cae3f1fd73c18ea72646030
Alex Hobson <aho113@uclive.ac.nz>
2021-05-04 11:36:59 +1200

    Added settings menu

--- app/src/main/java/seng440/vaccinepassport/MainActivity.kt
+++ app/src/main/java/seng440/vaccinepassport/MainActivity.kt
@@ -2,6 +2,10 @@ package seng440.vaccinepassport
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import seng440.vaccinepassport.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,23 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
+            //TODO show PIN/fingerprint unlock when set
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_settings -> {
+                Toast.makeText(this, "You did it!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
