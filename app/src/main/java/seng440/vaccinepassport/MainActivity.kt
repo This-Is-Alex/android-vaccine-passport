dad2e3791cc1e076d2843c3ac67ff6c3fd8c7a53
Alex Hobson <aho113@uclive.ac.nz>
2021-05-14 16:15:08 +1200

    Reintroduce main_activity.xml

--- app/src/main/java/seng440/vaccinepassport/MainActivity.kt
+++ app/src/main/java/seng440/vaccinepassport/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity(), BarcodeScannedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_fragment)
+        setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
