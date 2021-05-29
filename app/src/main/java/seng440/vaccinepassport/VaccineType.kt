b5e263427b2949a13d4ec44bd4e8ec3ecbbc9728
Rebekah McKinnon <rmc209@uclive.ac.nz>
2021-05-29 17:08:19 +1200

    Update VaccineType to also contain information about the number of doses and number of days of time between doses

--- app/src/main/java/seng440/vaccinepassport/VaccineType.kt
+++ app/src/main/java/seng440/vaccinepassport/VaccineType.kt
@@ -1,11 +1,11 @@
 package seng440.vaccinepassport
 
-enum class VaccineType(val id: Byte, val fullName: String) {
-    PFIZER(1, "Pfizer-BioNTech"),
-    JANSSEN(2, "Johnson & Johnson's Janssen"),
-    NOVAVAX(3, "NovaVax"),
-    ASTRA_ZENECA(4, "AstraZeneca"),
-    MODERNA(5, "Moderna");
+enum class VaccineType(val id: Byte, val fullName: String, val numDoses: Int, val daysBetweenDoses: Int?) {
+    PFIZER(1, "Pfizer-BioNTech", 2, 21),
+    JANSSEN(2, "Johnson & Johnson's Janssen", 1, null),
+    NOVAVAX(3, "NovaVax", 2, 31),
+    ASTRA_ZENECA(4, "AstraZeneca", 2, 56),
+    MODERNA(5, "Moderna", 2, 28);
 
     companion object {
         fun fromId(id: Byte): VaccineType? {
